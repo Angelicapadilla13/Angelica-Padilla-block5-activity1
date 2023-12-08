42f74a172e667274ba793dfc29094436b63a4382
Angelica F. Padilla <152839578+Angelicapadilla13@users.noreply.github.com>
2023-12-08 13:50:15 +0800

    Android Project

---
+++ app/src/main/java/com/padilla_angelica_block5_p1/quiz/MainActivity3.kt
@@ -0,0 +1,14 @@
+package com.padilla_angelica_block5_p1.quiz
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
